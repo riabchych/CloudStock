1c244f1c8d3f2809f977b9df1a03b592c13ef1eb
Yevhenii Riabchych <riabchych@gmail.com>
2018-02-10 08:39:21 +0200

    Added JerseyConfig

---
+++ src/main/java/com/riabchych/cloudstock/config/JerseyConfig.java
@@ -0,0 +1,15 @@
+package com.riabchych.cloudstock.config;
+
+import com.riabchych.cloudstock.endpoint.BarcodeEndpoint;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.ApplicationPath;
+
+@Component
+@ApplicationPath("/api")
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig() {
+        register(BarcodeEndpoint.class);
+    }
+}
