e60e6a980236b2c39899b8ca598e5ae053d2d8ea
Yevhenii Riabchych <riabchych@gmail.com>
2018-02-10 08:16:39 +0200

    Initial commit

---
+++ src/main/java/com/riabchych/cloudstock/CloudstockApplication.java
@@ -0,0 +1,12 @@
+package com.riabchych.cloudstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudstockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudstockApplication.class, args);
+    }
+}
