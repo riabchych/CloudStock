6af88147f6e7e507730280d877e900fd85fa9204
Yevhenii Riabchych <riabchych@gmail.com>
2018-02-16 18:47:53 +0200

    Added CrossOriginHeaders

---
+++ src/main/java/com/riabchych/cloudstock/security/CrossOriginHeaders.java
@@ -0,0 +1,15 @@
+package com.riabchych.cloudstock.security;
+
+public abstract class CrossOriginHeaders {
+
+    public static final String ACCESS_CONTROL_REQUEST_METHOD_HEADER = "Access-Control-Request-Method";
+    public static final String ACCESS_CONTROL_REQUEST_HEADERS_HEADER = "Access-Control-Request-Headers";
+
+    public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+
+    public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    public static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    public static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    public static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+
+}
