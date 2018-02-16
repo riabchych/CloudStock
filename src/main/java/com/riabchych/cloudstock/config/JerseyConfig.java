f8e03dd4a1afe152dbb5ca73b8170d3923506c5b
Yevhenii Riabchych <riabchych@gmail.com>
2018-02-16 18:26:48 +0200

    added new items

--- src/main/java/com/riabchych/cloudstock/config/JerseyConfig.java
+++ src/main/java/com/riabchych/cloudstock/config/JerseyConfig.java
@@ -1,7 +1,15 @@
 package com.riabchych.cloudstock.config;
 
+import com.riabchych.cloudstock.endpoint.AuthenticationEndpoint;
 import com.riabchych.cloudstock.endpoint.BarcodeEndpoint;
+import com.riabchych.cloudstock.endpoint.UserEndpoint;
+import com.riabchych.cloudstock.filter.AuthenticationFilter;
+import com.riabchych.cloudstock.filter.AuthorizationFilter;
+import com.riabchych.cloudstock.filter.CrossOriginResponseFilter;
+import com.riabchych.cloudstock.security.CrossOriginConfiguration;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.ApplicationPath;
@@ -9,7 +17,15 @@ import javax.ws.rs.ApplicationPath;
 @Component
 @ApplicationPath("/api")
 public class JerseyConfig extends ResourceConfig {
-    public JerseyConfig() {
+
+    @Autowired
+    public JerseyConfig(CrossOriginConfiguration crossOriginConfiguration) {
+        register(new CrossOriginResponseFilter(crossOriginConfiguration), 1);
+        register(RequestContextFilter.class);
         register(BarcodeEndpoint.class);
+        register(UserEndpoint.class);
+        register(AuthenticationFilter.class);
+        register(AuthorizationFilter.class);
+        register(AuthenticationEndpoint.class);
     }
 }
