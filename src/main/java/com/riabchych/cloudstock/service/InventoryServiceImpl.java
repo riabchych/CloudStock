e0e84273dfdf2142c1e862a7d126807c9136aeb6
Yevhenii Riabchych <riabchych@gmail.com>
2018-02-16 18:53:51 +0200

    added new service

---
+++ src/main/java/com/riabchych/cloudstock/service/InventoryServiceImpl.java
@@ -0,0 +1,35 @@
+package com.riabchych.cloudstock.service;
+
+import com.riabchych.cloudstock.entity.Inventory;
+
+import java.util.List;
+
+public class InventoryServiceImpl implements InventoryService {
+    @Override
+    public boolean addInventory(Inventory inventory) {
+        // TODO: Implement method
+        return false;
+    }
+
+    @Override
+    public Inventory getInventoryById(long id) {
+        // TODO: Implement method
+        return null;
+    }
+
+    @Override
+    public void updateInventory(Inventory inventory) {
+        // TODO: Implement method
+    }
+
+    @Override
+    public void deleteInventory(Long id) {
+        // TODO: Implement method
+    }
+
+    @Override
+    public List<Inventory> getAllInventorys() {
+        // TODO: Implement method
+        return null;
+    }
+}
