d653436db6a0c2ec6ddbc39ba55b246e6ab537bf
Yevhenii Riabchych <riabchych@gmail.com>
2018-02-16 18:55:59 +0200

    added new interface

--- src/main/java/com/riabchych/cloudstock/util/TokenUtil.java
+++ src/main/java/com/riabchych/cloudstock/util/TokenUtil.java
@@ -1,77 +1,22 @@
 package com.riabchych.cloudstock.util;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.apache.tomcat.util.buf.StringUtils;
+import com.riabchych.cloudstock.entity.Token;
+import com.riabchych.cloudstock.entity.User;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.security.Key;
-import java.util.Arrays;
 import java.util.Date;
 
-public class TokenUtil {
+public interface TokenUtil {
 
-    public static String getJWTString(String username, String[] roles, int version, Date expires, Key key) {
-        if (username == null) {
-            throw new NullPointerException("null username is illegal");
-        }
-        if (roles == null) {
-            throw new NullPointerException("null roles are illegal");
-        }
-        if (expires == null) {
-            throw new NullPointerException("null expires is illegal");
-        }
-        if (key == null) {
-            throw new NullPointerException("null key is illegal");
-        }
+    Token getJWTToken(String username, String[] roles);
 
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+    String getUsernameFromToken(String token);
 
-        return Jwts
-                .builder()
-                .setIssuer("Jersey-Security-Basic")
-                .setSubject(username)
-                .setAudience(StringUtils.join(Arrays.asList(roles), ','))
-                .setExpiration(expires)
-                .setIssuedAt(new Date())
-                .setId(String.valueOf(version))
-                .signWith(signatureAlgorithm, key)
-                .compact();
-    }
+    String getAudienceFromToken(String token);
 
-    public static boolean isValid(String token, Key key) {
-        try {
-            Jwts.parser().setSigningKey(key).parseClaimsJws(token.trim());
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+    Boolean canTokenBeRefreshed(String token, Date lastPasswordReset);
 
-    public static String getName(String jwsToken, Key key) {
-        if (isValid(jwsToken, key)) {
-            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(key).parseClaimsJws(jwsToken);
-            return claimsJws.getBody().getSubject();
-        }
-        return null;
-    }
+    String refreshToken(String token);
 
-    public static String[] getRoles(String jwsToken, Key key) {
-        if (isValid(jwsToken, key)) {
-            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(key).parseClaimsJws(jwsToken);
-            return claimsJws.getBody().getAudience().split(",");
-        }
-        return new String[]{};
-    }
-
-    public static int getVersion(String jwsToken, Key key) {
-        if (isValid(jwsToken, key)) {
-            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(key).parseClaimsJws(jwsToken);
-            return Integer.parseInt(claimsJws.getBody().getId());
-        }
-        return -1;
-    }
-
+    Boolean validateToken(String token, User user);
 }
